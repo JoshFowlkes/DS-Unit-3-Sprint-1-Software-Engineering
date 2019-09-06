1adb3c2bd64902f9fd5a5f56cc2b3b834ac391ec
Josh Fowlkes <52584954+JoshFowlkes@users.noreply.github.com>
2019-09-06 14:51:20 -0600

    fixed some typos

--- Sprint copy/acme.py
+++ Sprint copy/acme.py
@@ -3,7 +3,7 @@ import random
 
 """ Making The Product Class as outlined in Instructions """
 class Product:
-    def __init__(self, name, price=10, weight=20, flammability=.5, identifier):
+    def __init__(self, name, price=10, weight=20, flammability=.5):
         self.name = name
         self.price = price
         self.weight = weight
@@ -15,14 +15,14 @@ class Product:
         steal_ratio = self.price / self.weight
         if (steal_ratio < .05):
             return 'Not so stealable'
-        elif (steal_ratio > .5) and (.steal_ratio < 1.0):
+        elif (steal_ratio >= .5) and (steal_ratio < 1.0):
             return 'Kinda stealable'
         else:
             return 'Very Stealable!' 
     
     """ Making the Explode Function """
     def explode(self):
-        explode_ratio = self.flammability * 10
+        explode_ratio = self.flammability * self.weight
         if (explode_ratio < 10):
             return '...fizzle.'
         elif (explode_ratio >= 10) and (explode_ratio < 50):
@@ -34,16 +34,16 @@ class Product:
 """ Making the BoxingGlove Subclass of Product """
 class BoxingGlove(Product):
     def __init__(self, name):
-        super.__init__(self, name, weight=10)
+        super.__init__(name, weight=10)
     
     def explode(self):
         return "...it's a glove."
     
     """ Making the Punch Function  """
     def punch(self):
-        if (weight < 5):
+        if (self.weight < 5):
             return 'That tickles.'
-        elif (weight >= 5) and (weight < 15):
+        elif (self.weight >= 5) and (self.weight < 15):
             return 'Hey that hurt!'
         else:
             return 'OUCH!'
