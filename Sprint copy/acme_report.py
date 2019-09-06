4baedcfd0fa04bc4903a70103c17d82d592a165f
Josh Fowlkes <52584954+JoshFowlkes@users.noreply.github.com>
2019-09-06 11:42:19 -0600

    added comments for readability

--- Sprint copy/acme_report.py
+++ Sprint copy/acme_report.py
@@ -2,12 +2,15 @@
 from random import randint, sample, uniform
 from acme import Product
 
+""" Making lists of adjectives and nouns to be used as names """
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
+""" Generate Products functions """
 def generate_products(n=30):
     products = []
 
+    """ Making a for loop to make each individual product """
     for _ in list(range(n)):
         products = Product(name=(f'{random.sample(ADJECTIVES)} {random.sample(NOUNS)}')),
                         price = random.randint(5,100),
@@ -16,12 +19,15 @@ def generate_products(n=30):
                         identifier = random.randint(100000,999999))
     return products
 
+""" Making inventory report function """
 def inventory_report(products):
     print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
-
+    
+    """ Unique product names is len of names """
     unique_product_names = len(products.name)
     print('Unique product names:', unique_product_names)
 
+    """ Using mean() to find average of each then printing each one out """
     average_price = mean(products.price)
     print('Average price:', average_price)
 
